d314b1537bf5eecd8a2b844507069d38e0c6a7e6
marksansome <mark.s@roserocket.com>
2019-10-04 10:52:07 -0400

    changing imports to point at this repo

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jstemmer/go-junit-report/parser"
+	"github.com/RoseRocket/go-junit-report/parser"
 )
 
 // JUnitTestSuites is a collection of JUnit test suites.
